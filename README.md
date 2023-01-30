6ab742d782b96e82c4beca099448e831b72f11d3
Solntsev Maksim <solntsev1099@gmail.com>
2023-01-30 18:20:00 +0300

    refactoring: Проект разбит два модуля stats-service и ewm-service, внутри модуля stats-service созданы подмодули dto-library, содержащие в себе общие для stats-client и stats-server DTO. Внедряется в них как зависимость через pom.file add: BaseClient, HitClient, StatsClient для обработки запросов add: HitController, StatsController принимают запросы и передают их в сервис. add: StatsService обрабатывает запросы, запрашивает информацию в репозитории и возвращает ответ. add: HitRepository делает запросы в БД add: HitMapper трансформирует Entity в DTO и наоборот.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # java-explore-with-me
 Template repository for ExploreWithMe project.
+
+Для просмотра энд-поинтов сервиса статистики вы можете воспользоваться сваггером: 
+http://localhost:9090/swagger-ui/index.html#/
---
+++ stats-service/stats-server/src/main/java/ru/practicum/statservice/controllers/StatsController.java
@@ -0,0 +1,35 @@
+package ru.practicum.statservice.controllers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.ViewStats;
+import ru.practicum.statservice.service.StatsService;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/stats")
+public class StatsController {
+
+    private final StatsService statsService;
+
+    @GetMapping
+    public ArrayList<ViewStats> getStats(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                         @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+                                         @RequestParam (defaultValue = "") Collection<String> uris,
+                                         @RequestParam (defaultValue = "false") boolean unique) {
+        log.debug("A Get/stats?start={}&end={}&uris={}&flag={} request was received. Get stats about uris: {}", start,
+                end, uris, unique, uris);
+
+        return statsService.getStats(start, end, unique, uris);
+    }
+}
